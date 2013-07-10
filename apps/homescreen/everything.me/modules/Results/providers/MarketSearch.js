5e6f793e5567f9aa8a48c566ef8e75a54b5e8348
Amir Nissim <amirn@everything.me>
2013-07-10 18:04:45 +0300

    Marketplace result text --> "download more apps"

--- apps/homescreen/everything.me/modules/Results/providers/MarketSearch.js
+++ apps/homescreen/everything.me/modules/Results/providers/MarketSearch.js
@@ -1,6 +1,32 @@
 Evme.MarketSearchResult = function Evme_MarketSearch() {
+  var self = this,
+    TEXT_HEIGHT = Evme.Utils.APPS_FONT_SIZE * 3,
+    TEXT_WIDTH = 72 * Evme.Utils.devicePixelRatio,
+    TEXT_MARGIN = 6 * Evme.Utils.devicePixelRatio,
+
+    FONT_SIZE = 11 * Evme.Utils.devicePixelRatio;
+  
   Evme.Result.call(this);
   this.type = Evme.RESULT_TYPE.MARKET_SEARCH;
+
+  // @override
+  this.initIcon = function initIcon(baseHeight, textOffset) {
+    this.canvas.width = TEXT_WIDTH;
+    this.canvas.height = baseHeight + TEXT_MARGIN + (2 * TEXT_HEIGHT) - 1;
+    Evme.Utils.writeTextToCanvas({
+      "text": "Download",
+      "context": this.context,
+      "offset": textOffset + TEXT_MARGIN,
+      "fontSize": FONT_SIZE
+    });
+
+    Evme.Utils.writeTextToCanvas({
+      "text": "More Apps",
+      "context": this.context,
+      "offset": textOffset + TEXT_MARGIN + FONT_SIZE + 1 * Evme.Utils.devicePixelRatio
+    });
+  };
+
 }
 Evme.MarketSearchResult.prototype = Object.create(Evme.Result.prototype);
 Evme.MarketSearchResult.prototype.constructor = Evme.MarketSearchResult;
