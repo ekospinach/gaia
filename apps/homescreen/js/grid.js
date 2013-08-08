910ab9c6aedfa7433193a3f775134b136c268651
Ran Ben Aharon <ran@ranbena.com>
2013-08-08 19:27:05 +0200

    [Bug 902897] Prevent e.me to be scrolled while panning between pages (Vivien)

--- apps/homescreen/js/grid.js
+++ apps/homescreen/js/grid.js
@@ -288,6 +288,9 @@ var GridManager = (function() {
         // Generate a function accordingly to the current page position.
         if (currentPage > nextLandingPage || Homescreen.isInEditMode()) {
           var pan = function(e) {
+            e.preventDefault();
+            e.stopImmediatePropagation();
+
             currentX = getX(e);
             deltaX = panningResolver.getDeltaX(e);
 
@@ -320,6 +323,9 @@ var GridManager = (function() {
           }
 
           var pan = function(e) {
+            e.preventDefault();
+            e.stopImmediatePropagation();
+
             currentX = getX(e);
             deltaX = panningResolver.getDeltaX(e);
 
