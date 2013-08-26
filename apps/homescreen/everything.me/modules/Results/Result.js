42b9d017449dbf60a5c5d57e6a2700710ea790d8
Amir Nissim <amirn@everything.me>
2013-08-26 09:29:22 +0300

    tabs to spaces

--- apps/homescreen/everything.me/modules/Results/Result.js
+++ apps/homescreen/everything.me/modules/Results/Result.js
@@ -8,164 +8,164 @@ Evme.RESULT_TYPE = {
 };
 
 Evme.Result = function Evme_Result(__cfg, __index, __isMore) {
-	var NAME = "Result",
-		self = this,
-		cfg = {}, el = null,
-		index = __index,
-		isMore = __isMore,
+  var NAME = "Result",
+    self = this,
+    cfg = {}, el = null,
+    index = __index,
+    isMore = __isMore,
 
-		TEXT_HEIGHT = Evme.Utils.APPS_FONT_SIZE * 3,
-		TEXT_WIDTH = 72 * Evme.Utils.devicePixelRatio,
-		TEXT_MARGIN = 6 * Evme.Utils.devicePixelRatio,
+    TEXT_HEIGHT = Evme.Utils.APPS_FONT_SIZE * 3,
+    TEXT_WIDTH = 72 * Evme.Utils.devicePixelRatio,
+    TEXT_MARGIN = 6 * Evme.Utils.devicePixelRatio,
 
-		image = new Image();
+    image = new Image();
 
-	this.type = 'NOT_SET';
-	
-	this.app = null;
-	this.icon = null;
-	this.elIcon = null;
+  this.type = 'NOT_SET';
+  
+  this.app = null;
+  this.icon = null;
+  this.elIcon = null;
 
-	this.init = function init(app) {
-		cfg = app;
+  this.init = function init(app) {
+    cfg = app;
 
-		this.app = app;
+    this.app = app;
 
-		el = Evme.$create('li', {
-			'id': 'app_' + cfg.id,
-			'data-name': cfg.name
-		}, '<img />');
+    el = Evme.$create('li', {
+      'id': 'app_' + cfg.id,
+      'data-name': cfg.name
+    }, '<img />');
 
-		if ('isOfflineReady' in cfg) {
-			el.dataset.offlineReady = cfg.isOfflineReady;
-		}
+    if ('isOfflineReady' in cfg) {
+      el.dataset.offlineReady = cfg.isOfflineReady;
+    }
 
-		this.elIcon = el.querySelector('img');
+    this.elIcon = el.querySelector('img');
 
-		// remove button
-		if (app.isRemovable) {
-			var removeButton = Evme.$create('span', {
-				'class': 'remove' 
-			});
-			removeButton.addEventListener('click', cbRemoveClick);
-			removeButton.addEventListener('touchstart', stopPropagation);
-			removeButton.addEventListener('touchend', stopPropagation);
-			el.appendChild(removeButton);
-		}
+    // remove button
+    if (app.isRemovable) {
+      var removeButton = Evme.$create('span', {
+        'class': 'remove' 
+      });
+      removeButton.addEventListener('click', cbRemoveClick);
+      removeButton.addEventListener('touchstart', stopPropagation);
+      removeButton.addEventListener('touchend', stopPropagation);
+      el.appendChild(removeButton);
+    }
 
     el.addEventListener("click", onClick);
     el.addEventListener("contextmenu", onContextMenu);
 
-		return el;
-	};
+    return el;
+  };
 
-	this.draw = function draw(iconObj) {
-		cfg.icon = iconObj;
-		
-		if (el) {
-			el.setAttribute('data-name', cfg.name);
+  this.draw = function draw(iconObj) {
+    cfg.icon = iconObj;
+    
+    if (el) {
+      el.setAttribute('data-name', cfg.name);
 
-			if (Evme.Utils.isBlob(iconObj)) {
-				Evme.Utils.blobToDataURI(iconObj, function onDataReady(src) {
-					setImageSrc(src);
-				});
+      if (Evme.Utils.isBlob(iconObj)) {
+        Evme.Utils.blobToDataURI(iconObj, function onDataReady(src) {
+          setImageSrc(src);
+        });
 
-			} else {
-				var src  = Evme.Utils.formatImageData(iconObj);
-				setImageSrc(src);
-			}
-		}
+      } else {
+        var src  = Evme.Utils.formatImageData(iconObj);
+        setImageSrc(src);
+      }
+    }
 
-		function setImageSrc(src) {		
-			image.onload = self.onAppIconLoad;
-			image.src = src;
-		}
-	};
+    function setImageSrc(src) {   
+      image.onload = self.onAppIconLoad;
+      image.src = src;
+    }
+  };
 
-	/**
-	 * Save reference to the raw, unmaniputaled icon
-	 * Used when closing a collection to update its homescreen icon
-	 */
-	this.setIconSrc = function(src) {
-		el.dataset.iconSrc = src;
-	};
+  /**
+   * Save reference to the raw, unmaniputaled icon
+   * Used when closing a collection to update its homescreen icon
+   */
+  this.setIconSrc = function(src) {
+    el.dataset.iconSrc = src;
+  };
 
-	/**
-	 * Default implementation of Result icon rendering.
-	 * Currently only Evme.CloudAppResult implements different rendering.
-	 */
-	this.onAppIconLoad = function onAppIconLoad() {
-		// use OS icon rendering
-		var iconCanvas = Icon.prototype.createCanvas(image),
-				canvasSize = iconCanvas.width,
+  /**
+   * Default implementation of Result icon rendering.
+   * Currently only Evme.CloudAppResult implements different rendering.
+   */
+  this.onAppIconLoad = function onAppIconLoad() {
+    // use OS icon rendering
+    var iconCanvas = Icon.prototype.createCanvas(image),
+        canvasSize = iconCanvas.width,
 
-				canvas = self.initIcon(canvasSize, canvasSize),
-		    context = canvas.getContext('2d');
+        canvas = self.initIcon(canvasSize, canvasSize),
+        context = canvas.getContext('2d');
 
-		context.drawImage(iconCanvas, (TEXT_WIDTH - canvasSize) / 2, 0);
-		self.iconPostRendering(iconCanvas);
-		self.finalizeIcon(canvas);
-		self.setIconSrc(image.src);
-	};
+    context.drawImage(iconCanvas, (TEXT_WIDTH - canvasSize) / 2, 0);
+    self.iconPostRendering(iconCanvas);
+    self.finalizeIcon(canvas);
+    self.setIconSrc(image.src);
+  };
 
-	this.initIcon = function initIcon(baseHeight, textOffset) {
-	  var canvas = document.createElement('canvas'),
-	      context = canvas.getContext('2d');
+  this.initIcon = function initIcon(baseHeight, textOffset) {
+    var canvas = document.createElement('canvas'),
+        context = canvas.getContext('2d');
 
-		canvas.width = TEXT_WIDTH;
-		canvas.height = baseHeight + TEXT_MARGIN + TEXT_HEIGHT - 1;
+    canvas.width = TEXT_WIDTH;
+    canvas.height = baseHeight + TEXT_MARGIN + TEXT_HEIGHT - 1;
 
-		Evme.Utils.writeTextToCanvas({
-			"text": cfg.name,
-			"context": context,
-			"offset": textOffset + TEXT_MARGIN
-		});
+    Evme.Utils.writeTextToCanvas({
+      "text": cfg.name,
+      "context": context,
+      "offset": textOffset + TEXT_MARGIN
+    });
 
-		return canvas;
-	};
+    return canvas;
+  };
 
-	this.iconPostRendering = function iconPostRendering(iconCanvas) {
-		// default: do nothing
-	};
+  this.iconPostRendering = function iconPostRendering(iconCanvas) {
+    // default: do nothing
+  };
 
-	this.finalizeIcon = function finalizeIcon(canvas) {
-	  self.elIcon.src = canvas.toDataURL();
-	};
+  this.finalizeIcon = function finalizeIcon(canvas) {
+    self.elIcon.src = canvas.toDataURL();
+  };
 
-	this.remove = function remove() {
-		Evme.$remove(el);
-	};
+  this.remove = function remove() {
+    Evme.$remove(el);
+  };
 
-	this.isExternal = function isExternal() {
-		return cfg.isWeblink;
-	};
+  this.isExternal = function isExternal() {
+    return cfg.isWeblink;
+  };
 
-	this.getElement = function getElement() {
-		return el;
-	};
+  this.getElement = function getElement() {
+    return el;
+  };
 
-	this.getId = function getId() {
-		return cfg.id;
-	};
+  this.getId = function getId() {
+    return cfg.id;
+  };
 
-	this.getLink = function getLink() {
-		return cfg.appUrl;
-	};
+  this.getLink = function getLink() {
+    return cfg.appUrl;
+  };
 
-	this.getFavLink = function getFavLink() {
-		return cfg.favUrl != "@" && cfg.favUrl || cfg.appUrl;
-	};
+  this.getFavLink = function getFavLink() {
+    return cfg.favUrl != "@" && cfg.favUrl || cfg.appUrl;
+  };
 
-	this.getIcon = function getIcon() {
-		return cfg.icon;
-	};
+  this.getIcon = function getIcon() {
+    return cfg.icon;
+  };
 
-	this.getCfg = function getCfg() {
-		return cfg;
-	};
+  this.getCfg = function getCfg() {
+    return cfg;
+  };
 
-	function onClick(e) {
-		e.stopPropagation();
+  function onClick(e) {
+    e.stopPropagation();
 
     Evme.EventHandler.trigger(NAME, "click", {
       "app": self,
@@ -176,9 +176,9 @@ Evme.Result = function Evme_Result(__cfg, __index, __isMore) {
       "isMore": isMore,
       "e": e
     });
-	}
+  }
 
-	function onContextMenu(e) {
+  function onContextMenu(e) {
     e.stopPropagation();
     e.preventDefault();
 
@@ -190,16 +190,16 @@ Evme.Result = function Evme_Result(__cfg, __index, __isMore) {
       "index": index,
       "isMore": isMore
     });
-	}
+  }
 
-	// prevent app click from being triggered
-	function stopPropagation(e) {
-		e.stopPropagation();
-	}
+  // prevent app click from being triggered
+  function stopPropagation(e) {
+    e.stopPropagation();
+  }
 
-	function cbRemoveClick() {
-		Evme.EventHandler.trigger(NAME, "remove", {
-			"id": cfg.id
-		});
-	}
+  function cbRemoveClick() {
+    Evme.EventHandler.trigger(NAME, "remove", {
+      "id": cfg.id
+    });
+  }
 }
--- apps/homescreen/everything.me/modules/Results/providers/CloudApps.js
+++ apps/homescreen/everything.me/modules/Results/providers/CloudApps.js
@@ -1,48 +1,48 @@
 Evme.CloudAppResult = function Evme_CloudAppsResult() {
-	Evme.Result.call(this);
+  Evme.Result.call(this);
 
-	this.type = Evme.RESULT_TYPE.CLOUD;
+  this.type = Evme.RESULT_TYPE.CLOUD;
 
-	var self = this,
-			SHADOW_OFFSET = 2 * Evme.Utils.devicePixelRatio,
-			SHADOW_BLUR = 2 * Evme.Utils.devicePixelRatio,
-			SIZE = 52 * Evme.Utils.devicePixelRatio,
-			FULL_SIZE = SIZE + SHADOW_OFFSET + SHADOW_BLUR;
+  var self = this,
+      SHADOW_OFFSET = 2 * Evme.Utils.devicePixelRatio,
+      SHADOW_BLUR = 2 * Evme.Utils.devicePixelRatio,
+      SIZE = 52 * Evme.Utils.devicePixelRatio,
+      FULL_SIZE = SIZE + SHADOW_OFFSET + SHADOW_BLUR;
 
-	
-	// @override
-	// manipulate the icon (clipping, shadow, resize)
-	this.onAppIconLoad = function CloudResult_onAppIconLoad() {
-		var canvas = self.initIcon(FULL_SIZE, SIZE),
-		    context = canvas.getContext('2d'),
+  
+  // @override
+  // manipulate the icon (clipping, shadow, resize)
+  this.onAppIconLoad = function CloudResult_onAppIconLoad() {
+    var canvas = self.initIcon(FULL_SIZE, SIZE),
+        context = canvas.getContext('2d'),
 
-		    elImageCanvas = document.createElement('canvas'),
-		    imageContext = elImageCanvas.getContext('2d'),
-		    fixedImage = new Image();
+        elImageCanvas = document.createElement('canvas'),
+        imageContext = elImageCanvas.getContext('2d'),
+        fixedImage = new Image();
 
-		elImageCanvas.width = elImageCanvas.height = FULL_SIZE;
-		
-		imageContext.beginPath();
-		imageContext.arc(FULL_SIZE / 2, FULL_SIZE / 2, SIZE / 2, 0, Math.PI * 2, false);
-		imageContext.closePath();
-		imageContext.clip();
-		imageContext.drawImage(this, (FULL_SIZE - SIZE) / 2, (FULL_SIZE - SIZE) / 2, SIZE, SIZE);
+    elImageCanvas.width = elImageCanvas.height = FULL_SIZE;
+    
+    imageContext.beginPath();
+    imageContext.arc(FULL_SIZE / 2, FULL_SIZE / 2, SIZE / 2, 0, Math.PI * 2, false);
+    imageContext.closePath();
+    imageContext.clip();
+    imageContext.drawImage(this, (FULL_SIZE - SIZE) / 2, (FULL_SIZE - SIZE) / 2, SIZE, SIZE);
 
-		// save a reference to the clipped icon
-		self.setIconSrc(elImageCanvas.toDataURL());
+    // save a reference to the clipped icon
+    self.setIconSrc(elImageCanvas.toDataURL());
 
-		fixedImage.onload = function onImageLoad() {
-		    // shadow
-			context.shadowOffsetX = 0;
-			context.shadowOffsetY = SHADOW_OFFSET;
-			context.shadowBlur = SHADOW_BLUR;
-			context.shadowColor = 'rgba(0, 0, 0, 0.6)';
-	    context.drawImage(fixedImage, (canvas.width - FULL_SIZE) / 2, 0);
-	    self.finalizeIcon(canvas);
-		};
+    fixedImage.onload = function onImageLoad() {
+        // shadow
+      context.shadowOffsetX = 0;
+      context.shadowOffsetY = SHADOW_OFFSET;
+      context.shadowBlur = SHADOW_BLUR;
+      context.shadowColor = 'rgba(0, 0, 0, 0.6)';
+      context.drawImage(fixedImage, (canvas.width - FULL_SIZE) / 2, 0);
+      self.finalizeIcon(canvas);
+    };
 
-		fixedImage.src = elImageCanvas.toDataURL('image/png');
-	};
+    fixedImage.src = elImageCanvas.toDataURL('image/png');
+  };
 };
 
 Evme.CloudAppResult.prototype = Object.create(Evme.Result.prototype);
@@ -50,143 +50,143 @@ Evme.CloudAppResult.prototype.constructor = Evme.CloudAppResult;
 
 
 Evme.CloudAppsRenderer = function Evme_CloudAppsRenderer() {
-	var NAME = 'CloudAppsRenderer',
-		self = this,
-		containerEl,
-		lastRenderedResults = {}, // app.id -> Result instance
-		lastSignature = Evme.Utils.EMPTY_APPS_SIGNATURE,
-		iconFormat = Evme.Utils.getIconsFormat(),	
-		defaultIconIndex = 0,
+  var NAME = 'CloudAppsRenderer',
+    self = this,
+    containerEl,
+    lastRenderedResults = {}, // app.id -> Result instance
+    lastSignature = Evme.Utils.EMPTY_APPS_SIGNATURE,
+    iconFormat = Evme.Utils.getIconsFormat(), 
+    defaultIconIndex = 0,
 
-		DEFAULT_ICON_URLS = Evme.Config.design.apps.defaultIconUrl[Evme.Utils.ICONS_FORMATS.Large];
+    DEFAULT_ICON_URLS = Evme.Config.design.apps.defaultIconUrl[Evme.Utils.ICONS_FORMATS.Large];
 
 
-	this.init = function init(cfg) {
-		containerEl = cfg.containerEl;
-	};
+  this.init = function init(cfg) {
+    containerEl = cfg.containerEl;
+  };
 
-	this.render = function render(apps, pageNum, missingIconsCb) {
-		if (!apps.length) return;
+  this.render = function render(apps, pageNum, missingIconsCb) {
+    if (!apps.length) return;
 
-		var	newSignature = Evme.Utils.getAppsSignature(apps);
+    var newSignature = Evme.Utils.getAppsSignature(apps);
 
-		// if same apps as last - do nothing
-		if (lastSignature === newSignature) {
-			Evme.Utils.log("CloudAppsRenderer: nothing to render (signature match)");
-			return;
-		}
-		lastSignature = newSignature;
+    // if same apps as last - do nothing
+    if (lastSignature === newSignature) {
+      Evme.Utils.log("CloudAppsRenderer: nothing to render (signature match)");
+      return;
+    }
+    lastSignature = newSignature;
 
-		// if not "loaded more", clear current results
-		if (pageNum === 0) {
-			self.clear();
-		}
+    // if not "loaded more", clear current results
+    if (pageNum === 0) {
+      self.clear();
+    }
 
-		_render(apps, missingIconsCb);
-	};
+    _render(apps, missingIconsCb);
+  };
 
-	this.clear = function clear() {
-		containerEl.innerHTML = '';
+  this.clear = function clear() {
+    containerEl.innerHTML = '';
     lastRenderedResults = {};
     lastSignature = Evme.Utils.EMPTY_APPS_SIGNATURE;
-		defaultIconIndex = 0;
-	};
+    defaultIconIndex = 0;
+  };
 
-	/*
-		data = [{ id: id, icon: {} }, ... ]
-	*/
-	this.updateIcons = function updateIcons(data) {
-		for (var i=0, entry; entry=data[i++];){
-		  var result = lastRenderedResults[entry.id];
-		  result && result.draw(entry.icon);
-		}
-	};
+  /*
+    data = [{ id: id, icon: {} }, ... ]
+  */
+  this.updateIcons = function updateIcons(data) {
+    for (var i=0, entry; entry=data[i++];){
+      var result = lastRenderedResults[entry.id];
+      result && result.draw(entry.icon);
+    }
+  };
 
-	this.getResultCount = function getResultCount() {
-		return containerEl.childElementCount;
-	};
+  this.getResultCount = function getResultCount() {
+    return containerEl.childElementCount;
+  };
 
-	function _render(apps, missingIconsCb){
-		var docFrag = document.createDocumentFragment(),
-			noIconAppIds = [];  // ids of apps received without an icon
+  function _render(apps, missingIconsCb){
+    var docFrag = document.createDocumentFragment(),
+      noIconAppIds = [];  // ids of apps received without an icon
 
-		for (var i = 0, app; app = apps[i++];) {
-			app.isOfflineReady = false;
-			
-			var result = new Evme.CloudAppResult(),
-					el = result.init(app);
+    for (var i = 0, app; app = apps[i++];) {
+      app.isOfflineReady = false;
+      
+      var result = new Evme.CloudAppResult(),
+          el = result.init(app);
 
-			if (app.icon) {  // app with icon url from API response
-				result.draw(app.icon);
-				Evme.IconManager.add(app.id, app.icon, iconFormat);
+      if (app.icon) {  // app with icon url from API response
+        result.draw(app.icon);
+        Evme.IconManager.add(app.id, app.icon, iconFormat);
 
-			} else if (isWebLink(app)) {  // no id for weblinks so generate one
-				app.id = 'app-' + Evme.Utils.uuid();
-				app.icon = getDefaultIcon();
-				result.draw(app.icon);
+      } else if (isWebLink(app)) {  // no id for weblinks so generate one
+        app.id = 'app-' + Evme.Utils.uuid();
+        app.icon = getDefaultIcon();
+        result.draw(app.icon);
 
-			} else {  // icon will be drawn from cache (or requested if missing)
-				noIconAppIds.push(app.id);
-			}
+      } else {  // icon will be drawn from cache (or requested if missing)
+        noIconAppIds.push(app.id);
+      }
 
-			// used for result filtering
-			result.getElement().dataset.url = app.appUrl;
+      // used for result filtering
+      result.getElement().dataset.url = app.appUrl;
 
-			lastRenderedResults[app.id] = result;
+      lastRenderedResults[app.id] = result;
 
-			docFrag.appendChild(el);
-		}
+      docFrag.appendChild(el);
+    }
 
-		containerEl.appendChild(docFrag);
+    containerEl.appendChild(docFrag);
 
-		noIconAppIds.length && getCachedIconsAsync(noIconAppIds, missingIconsCb);
-	}
+    noIconAppIds.length && getCachedIconsAsync(noIconAppIds, missingIconsCb);
+  }
 
-	function getCachedIconsAsync(appIds, missingIconsCb) {
-		var idsMissing = [], // ids of apps which have no cached icon
-			pendingRequests = appIds.length;
+  function getCachedIconsAsync(appIds, missingIconsCb) {
+    var idsMissing = [], // ids of apps which have no cached icon
+      pendingRequests = appIds.length;
 
-		for (var i=0, appId; appId=appIds[i++];) {
-			_getCachedIcon(appId);
-		}
+    for (var i=0, appId; appId=appIds[i++];) {
+      _getCachedIcon(appId);
+    }
 
-		// wrapped in function to create new scope (with correct value of appId)
-		function _getCachedIcon(appId) {
-			Evme.IconManager.get(appId, function onIconFromCache(iconFromCache) {
-				// make sure app still appears in results
-				var app = lastRenderedResults[appId];
-				if (!app) {
-				  return;
-			  }
+    // wrapped in function to create new scope (with correct value of appId)
+    function _getCachedIcon(appId) {
+      Evme.IconManager.get(appId, function onIconFromCache(iconFromCache) {
+        // make sure app still appears in results
+        var app = lastRenderedResults[appId];
+        if (!app) {
+          return;
+        }
 
-				if (iconFromCache) {
+        if (iconFromCache) {
           app.icon = iconFromCache;
-					app.draw(iconFromCache);
-				} else {
-					idsMissing.push(appId);
-					app.draw(getDefaultIcon());
-				}
+          app.draw(iconFromCache);
+        } else {
+          idsMissing.push(appId);
+          app.draw(getDefaultIcon());
+        }
 
-				pendingRequests--;
+        pendingRequests--;
 
-				// all cache requests returned - request missing icons
-				if (pendingRequests === 0) {
-					idsMissing.length && missingIconsCb(idsMissing);
-				}
-			});
-		}
+        // all cache requests returned - request missing icons
+        if (pendingRequests === 0) {
+          idsMissing.length && missingIconsCb(idsMissing);
+        }
+      });
+    }
 
-	}
+  }
 
-	function isWebLink(app){
-		// apps that are not indexed by E.me (web links)
-		// or missing id for some reason
-		return app.isWebLink || app.type === Evme.RESULT_TYPE.WEBLINK || !app.id;
-	}
+  function isWebLink(app){
+    // apps that are not indexed by E.me (web links)
+    // or missing id for some reason
+    return app.isWebLink || app.type === Evme.RESULT_TYPE.WEBLINK || !app.id;
+  }
 
-	function getDefaultIcon() {
-		var defaultIcon = DEFAULT_ICON_URLS[defaultIconIndex];
-		defaultIconIndex = (defaultIconIndex + 1) % DEFAULT_ICON_URLS.length;
-		return defaultIcon;
-	}
+  function getDefaultIcon() {
+    var defaultIcon = DEFAULT_ICON_URLS[defaultIconIndex];
+    defaultIconIndex = (defaultIconIndex + 1) % DEFAULT_ICON_URLS.length;
+    return defaultIcon;
+  }
 };
