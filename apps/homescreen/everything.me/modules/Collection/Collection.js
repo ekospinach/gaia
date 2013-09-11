2acfa5fe2ef00951fe6de5f07d3db59c4438f351
Amir Nissim <amirn@everything.me>
2013-09-11 11:39:11 +0200

    Bug 914875 - [e.me][bug] v1.2 homescreen performance

--- apps/homescreen/everything.me/modules/Collection/Collection.js
+++ apps/homescreen/everything.me/modules/Collection/Collection.js
@@ -43,6 +43,18 @@
       elClose.addEventListener('click', self.hide);
       elAppsContainer.dataset.scrollOffset = 0;
 
+      el.addEventListener('animationend', function onAnimationEnd(e) {
+        if (e.animationName === 'collection-hide') {
+          el.style.display = 'none';
+        }
+      });
+
+      el.addEventListener('animationstart', function onAnimationStart(e) {
+        if (e.animationName === 'collection-show') {
+          el.style.display = '';  
+        }
+      });
+
       Evme.EventHandler.trigger(NAME, 'init');
     };
 
